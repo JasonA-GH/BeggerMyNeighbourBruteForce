7175179d5e0f4b688a079a0cf69b2b465c06fa58
JasonA-GH <136386469+JasonA-GH@users.noreply.github.com>
2025-04-01 14:50:02 -0400

    Update start.c

--- start.c
+++ start.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int* cards;
 
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void randomize_deck()
+{
+    for(int i=0; i < 52; i++)
+    {
+        int r = rand()%52;
+        swap(&(cards[i]), &(cards[r]));
+        //cards[i] = 
+    }
+}
+
+void play_game_until_end()
+{
+    
+}
+
 int main()
 {
+    srand(time(NULL));
     cards = malloc(sizeof(*cards)*52);
     
     for(int i=0; i < 36; i++)
@@ -19,6 +43,8 @@ int main()
         if(index == 5)
             index = 1;
     }
+    
+    play_game_until_end();
     
     for(int i=0; i < 52; i++)
     {
